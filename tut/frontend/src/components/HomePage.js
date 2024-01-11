c5cdefab012abd54f9c919bbbdeb439b4528c5e6
Mateussszzzzz <130097512+Mateussszzzzz@users.noreply.github.com>
2024-01-11 12:32:43 +0000

    Crad

--- tut/frontend/src/components/HomePage.js
+++ tut/frontend/src/components/HomePage.js
@@ -10,6 +10,7 @@ import {
 } from "react-router-dom";
 import CustomDrawer from "./CustomDrawer"
 import SettingsSection from "./SettingsSection";
+import CardView from "./CardView";
 
 export default class HomePage extends Component {
   constructor(props) {
@@ -22,6 +23,7 @@ export default class HomePage extends Component {
         <Switch>
           <Route exact path="/">
             <CustomDrawer />
+            <CardView style={{left: '50%', top: '50%'}}/>
           </Route>
           <Route path="/account" component={AccountPage} />
           <Route path="/create" component={CreateNotePage} />
