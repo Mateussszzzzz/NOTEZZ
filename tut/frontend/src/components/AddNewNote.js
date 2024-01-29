8a173a68bf4146744e5f6dd3e45eb4099e7b7f02
Mateussszzzzz <130097512+Mateussszzzzz@users.noreply.github.com>
2024-01-29 18:41:37 +0000

    juz prawie jest frontend czas na backend i impleme

--- tut/frontend/src/components/AddNewNote.js
+++ tut/frontend/src/components/AddNewNote.js
@@ -2,7 +2,9 @@ import * as React from 'react';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
-import  IconButton  from '@mui/material/IconButton';
+import IconButton  from '@mui/material/IconButton';
+import CardActions from '@mui/material/CardActions';
+import Typography  from '@mui/material/Typography';
 
 
 
@@ -11,11 +13,22 @@ export default function AddNewNote(props){
     return(
         
             <Card sx={{border: 3, borderColor: 'green', borderStyle: 'dashed', height: 200 , width: 200}}>
-            <CardContent>
-            <IconButton sx={{left: '50%', top: '50%', transform: 'translate(-50%, -50%))'}}>
-                <AddCircleOutlineIcon color='success' />
+                <CardActions sx={{
+                            position: 'relative', 
+                            left: '80%', 
+                            top: '50%',
+                            transform: 'translate(-50%, -50%)'}}>
+                <IconButton size='large'>
+                <AddCircleOutlineIcon color='success' fontSize='inherit'/>
                 </IconButton>
-            </CardContent>
+                </CardActions>
+                <CardContent sx={{position: 'relative', 
+                            left: '65%', 
+                            top: '50%',
+                            transform: 'translate(-50%, -50%)'}}>
+                <Typography color={'green'}> Add new note</Typography>
+                </CardContent>
+            
             </Card>
     )
 }
--- tut/frontend/src/components/HomePage.js
+++ tut/frontend/src/components/HomePage.js
@@ -12,6 +12,8 @@ import CustomDrawer from "./CustomDrawer"
 import SettingsSection from "./SettingsSection";
 import CardView from "./CardView";
 import AddNewNote from "./AddNewNote";
+import Grid2 from "@mui/material/Unstable_Grid2/Grid2";
+import  Box from "@mui/material/Box";
 
 export default class HomePage extends Component {
   constructor(props) {
@@ -24,8 +26,12 @@ export default class HomePage extends Component {
         <Switch>
           <Route exact path="/">
             <CustomDrawer />
-            <CardView />
-            <AddNewNote />
+            <Box sx={{position: 'relative', left: '50%', top: '10%', transform: 'translate(-50%, -50%))'}}>
+            <Grid2 container spacing={10}>
+              <Grid2 xs={6}><CardView /></Grid2>
+              <Grid2 xs={6}><AddNewNote />  </Grid2>           
+            </Grid2>
+            </Box>
           </Route>
           <Route path="/account" component={AccountPage} />
           <Route path="/create" component={CreateNotePage} />
