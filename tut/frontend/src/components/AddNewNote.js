f5302aa8fa34eda23cdc85d68667091d336d9246
Mateussszzzzz <130097512+Mateussszzzzz@users.noreply.github.com>
2024-02-13 14:49:50 +0000

    dobra dziala w miare

--- tut/frontend/src/components/AddNewNote.js
+++ tut/frontend/src/components/AddNewNote.js
@@ -1,34 +1,67 @@
 import * as React from 'react';
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+import  IconButton  from '@mui/material/IconButton';
 import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
-import IconButton  from '@mui/material/IconButton';
-import CardActions from '@mui/material/CardActions';
-import Typography  from '@mui/material/Typography';
 
+export default function FormDialog() {
+  const [open, setOpen] = React.useState(false);
 
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
 
+  const handleClose = () => {
+    setOpen(false);
+  };
 
-export default function AddNewNote(props){
-    return(
-        
-            <Card sx={{border: 3, borderColor: 'green', borderStyle: 'dashed', height: 200 , width: 200}}>
-                <CardActions sx={{
-                            position: 'relative', 
-                            left: '80%', 
-                            top: '50%',
-                            transform: 'translate(-50%, -50%)'}}>
-                <IconButton size='large'>
-                <AddCircleOutlineIcon color='success' fontSize='inherit'/>
-                </IconButton>
-                </CardActions>
-                <CardContent sx={{position: 'relative', 
-                            left: '65%', 
-                            top: '50%',
-                            transform: 'translate(-50%, -50%)'}}>
-                <Typography color={'green'}> Add new note</Typography>
-                </CardContent>
-            
-            </Card>
-    )
+  return (
+    <React.Fragment>
+      <IconButton variant="outlined" onClick={handleClickOpen} color='success' size='large'>
+        <AddCircleOutlineIcon fontSize='large'/>
+      </IconButton>
+      <Dialog
+        open={open}
+        onClose={handleClose}
+        PaperProps={{
+          component: 'form',
+          onSubmit: (event) => {
+            event.preventDefault();
+            const formData = new FormData(event.currentTarget);
+            const formJson = Object.fromEntries(formData.entries());
+            const email = formJson.email;
+            console.log(email);
+            handleClose();
+          },
+        }}
+      >
+        <DialogTitle>Subscribe</DialogTitle>
+        <DialogContent>
+          <DialogContentText>
+            To create a new note you need to first create title for it.
+          </DialogContentText>
+          <TextField
+            autoFocus
+            required
+            margin="dense"
+            id="name"
+            name="title"
+            label="Title"
+            type="title"
+            fullWidth
+            variant="standard"
+          />
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose}>Cancel</Button>
+          <Button type="submit">Create</Button>
+        </DialogActions>
+      </Dialog>
+    </React.Fragment>
+  );
 }
